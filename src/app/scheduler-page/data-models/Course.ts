b307c9a1859131bffbb475f8cd02bf1666333ed1
SM <jsantamarina919@gmail.com>
2025-09-14 11:23:56 -0700

    remove semester available and semester planned from course object

--- src/app/scheduler-page/data-models/Course.ts
+++ src/app/scheduler-page/data-models/Course.ts
@@ -4,8 +4,6 @@ export class Course {
     public id: string,
     public name: string,
     public units: number,
-    public semesterPlanned: number | null,
-    public semesterAvailable: number | null,
   ) {
   }
 
